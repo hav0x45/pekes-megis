9408e27f9ac2f9069d2e634212b42689805a113f
hav0x45 <pxksii@gmail.com>
2022-04-19 14:01:30 +0300

    Added the new item

--- src/main/java/net/pekes/megis/PekesMegis.java
+++ src/main/java/net/pekes/megis/PekesMegis.java
@@ -1,6 +1,10 @@
 package net.pekes.megis;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +14,16 @@ public class PekesMegis implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("pekesmegis");
 
+	public static final Item MEGIS = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier("pekesmegis", "megis"), MEGIS);
+
 
 		LOGGER.info("Hello Fabric world!");
 	}
