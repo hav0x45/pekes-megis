734518b0f25fa0ebd2d21480248875057e0a4c4f
hav0x45 <pxksii@gmail.com>
2022-04-19 13:38:05 +0300

    Initial commit - Mod Base

---
+++ src/main/java/net/pekes/megis/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.pekes.megis.mixin;
+
+import net.pekes.megis.PekesMegis;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		PekesMegis.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
